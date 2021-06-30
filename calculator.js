bbfe2c3cf4d7fcf9c3b1130464feb2963dfd6026
Phil Smithies <philsmithies@gmail.com>
2021-06-30 19:42:59 +0200

    calculator and tests

---
+++ calculator.js
@@ -0,0 +1,8 @@
+function Calculator(a, b) {
+  this.sum = function(a, b) {return a + b};
+  this.subtract = function(a, b) {return a - b};
+  this.multiply = function(a, b) {return a * b};
+  this.divide = function(a, b) {return a / b};
+}
+
+module.exports = Calculator;
